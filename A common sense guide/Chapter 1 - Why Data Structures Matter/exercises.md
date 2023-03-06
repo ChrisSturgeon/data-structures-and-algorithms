312798de42c428be350b024675074419eb549f2e
csturgeon <sturgeon.chris@gmail.com>
2023-03-06 05:12:03 +0000

    Add test answers to Chapter 1 exercises

---
+++ A common sense guide/Chapter 1 - Why Data Structures Matter/exercises.md
@@ -0,0 +1,17 @@
+# Chapter 1 Exercises
+
+### Question 1
+
+For an array containing 100 elements, provide the number of steps the following operations would take:
+
+a) Reading: **1** step.
+
+b) Searching for a value not contained within the array: **100** steps.
+
+c) Insertion at the beginning of the array: 100 steps to shift existing elements to the right in memory to create gap, and an 1 step to insert the new element = **101** steps.
+
+d) Insertion at the end of the array: **1** step.
+
+e) Deletion at the beginning of the array: 1 step to delete the element and 99 steps to shift remaining elements left to close the gap = **100** steps.
+
+f) Deletion at the end of the array: **1** step.
---
+++ A common sense guide/Chapter 2 - Why algorithms matter/binary-search.js
@@ -0,0 +1,33 @@
+function binarySearch(arr, searchValue) {
+  // Create upper and lower bound pointers
+  let lowerPointer = 0;
+  let upperPointer = arr.length - 1;
+
+  while (lowerPointer <= upperPointer) {
+    // Find the midpoint between the two pointers
+    let midPoint = Math.floor((lowerPointer + upperPointer) / 2);
+
+    // Lookup the array value at the midpoints
+    let valueAtMidPoint = arr[midPoint];
+
+    // Return the midpoint if it matches the required value
+    if (arr[midPoint] === searchValue) {
+      return `${searchValue} found at array index ${midPoint}`;
+    }
+
+    // If the search value is smaller than the midpoint, move the upperbound pointer to just below the current midpoint
+    if (searchValue < valueAtMidPoint) {
+      upperPointer = midPoint - 1;
+    }
+
+    // If the search value is greater than the midpoint, move the lowerbound pointer to just above the current midpoint
+    if (searchValue > valueAtMidPoint) {
+      lowerPointer = midPoint + 1;
+    }
+  }
+
+  // Return a not found message if not present;
+  return `${searchValue} not found in array`;
+}
+
+console.log(binarySearch([3, 17, 75, 80, 202], 80));
